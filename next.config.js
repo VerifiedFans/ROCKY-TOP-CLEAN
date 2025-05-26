9b720453e1792eb4df1125a72faa1803eca51432
VerifiedFans <troy@knoxconcepts.com>
2025-05-25 21:11:31 -0400

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+  }
+};
+
+module.exports = nextConfig;
